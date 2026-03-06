a878e4ac03ab94ea77844dfc5d80c3eeb7bd0111
Shota <tennten266@gmail.com>
2026-03-06 20:27:42 +0900

    Add files via upload

---
+++ 1-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+//2 hello world
+{
+    printf("H   H EEEEE L     PPPP  MMM MMM EEEEE\n");
+    printf("H   H E     L     P   P M M M M E    \n");
+    printf("HHHHH EEEE  L     PPPP  M  M  M EEEE \n");
+    printf("H   H E     L     P     M     M E    \n");
+    printf("H   H EEEEE LLLLL P     M     M EEEEE\n");
+}
